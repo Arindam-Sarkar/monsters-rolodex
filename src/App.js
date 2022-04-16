f3f7d6ce57d3803c8063cdbd1783a9937249bbd0
Arindam <arindamsocial85@gmail.com>
2022-04-16 11:31:54 +0530

    Blank Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+      searchString: ""
+    }
+  }
+
+  onSearchChange = str => {
+    this.setState({ searchString: str.target.value })
+  }
+
+  render() {
+
+    return (
+      <div className='App'>
+        < input type="search"
+          placeholder='Enter Monsters'
+          onChange={this.onSearchChange}
+        />
+
+        <h1>{this.state.searchString}</h1>
+      </div>
+
+
+    )
+  }
+
 }
 
 export default App;
